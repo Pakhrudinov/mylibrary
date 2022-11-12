acf0164d02d7e08e93fe5390234e8f61f5e9db70
Saygid <saygidp@mail.ru>
2022-11-13 00:42:24 +0300

    add generic and detail views for authors

--- catalog/views.py
+++ catalog/views.py
@@ -33,7 +33,16 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
