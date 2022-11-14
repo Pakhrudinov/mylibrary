97b1ee8ce2ea886fce40effc0c10b9616add9f0c
Saygid <saygidp@mail.ru>
2022-11-14 22:18:56 +0300

    added use of sessions

--- catalog/views.py
+++ catalog/views.py
@@ -10,25 +10,32 @@ def index(request):
     """
     Функция отображения для домашней страницы сайта.
     """
+    # Количество посещений этого представления, подсчитанное в переменной сеанса.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Генерация "количеств" некоторых главных объектов
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_genre = Genre.objects.all().count()
     num_best_books = Book.objects.filter(title__icontains='Best').count()
+
     # Доступные книги (статус = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
-    return render(
-        request,
-        'index.html',
-        context={
-            'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available,
-            'num_authors': num_authors, 'num_genre': num_genre, 'num_best_books': num_best_books,
-        },
-    )
+    context = {
+                  'num_books': num_books,
+                  'num_instances': num_instances,
+                  'num_instances_available': num_instances_available,
+                  'num_authors': num_authors,
+                  'num_genre': num_genre,
+                  'num_best_books': num_best_books,
+                  'num_visits': num_visits,
+              }
+    return render(request, 'index.html', context=context)
 
 
 class BookListView(generic.ListView):
